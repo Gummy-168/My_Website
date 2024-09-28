72c636016ec788eb05e54935d652d139d5d9b36b
Gummy-168 <titikab55@gmail.com>
2024-09-28 15:08:25 +0700

    Edit all file and setup

--- all.js
+++ all.js
@@ -11,3 +11,16 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+  
+      const targetId = this.getAttribute('href');
+      const targetElement = document.querySelector(targetId);
+  
+      targetElement.scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
